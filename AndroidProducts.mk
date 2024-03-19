2823e2079539a983e1ad6201e9474d57f3b06c6a
drpto <dr.juniornoob@gmail.com>
2024-03-19 12:53:44 +0530

    stone: Gi with Cipher OS this time
    Signed-off-by: drpto <dr.juniornoob@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_stone.mk
+    $(LOCAL_DIR)/cipher_stone.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_stone-user \
-    lineage_stone-userdebug \
-    lineage_stone-eng
+    cipher_stone-user \
+    cipher_stone-userdebug \
+    cipher_stone-eng
--- cipher_stone.mk
+++ cipher_stone.mk
@@ -9,13 +9,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cipher/config/common_full_phone.mk)
 
 # Inherit from stone device
 $(call inherit-product, device/xiaomi/stone/device.mk)
 
 # Device props
-TARGET_SUPPORTS_BLUR := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_DISABLE_EPPE := true
@@ -24,11 +23,15 @@ TARGET_DEBLOAT := true
 # Charging Animation
 TARGET_USE_PIXEL_CHARGER := true
 
-# Use Prebuilt Kernel
-PREBUILT_KERNEL := true
+# CipherOS specific flag
+CIPHER_MAINTAINER := Dr.Opto
+CIPHER_GAPPS := true
+CIPHER_BATTERY := 5000
+CIPHER_SCREEN :=  6.67
+CIPHER_BLUR := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_stone
+PRODUCT_NAME := cipher_stone
 PRODUCT_DEVICE := stone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
