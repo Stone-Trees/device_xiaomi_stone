818e956f9ad371befc526f2721ed68e657c0bb68
drpto <dr.juniornoob@gmail.com>
2024-03-19 12:49:36 +0530

    stone: Track vendor from https://github.com/Amrito-Projects/vendor_xiaomi.git & Kernel from our ORG
    Signed-off-by: drpto <dr.juniornoob@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,9 @@
 git clone --depth=1 https://github.com/Stone-Trees/device_xiaomi_sm6375-common.git -b lineage-21 device/xiaomi/sm6375-common
 
-git clone --depth=1 https://github.com/Stone-Trees/vendor_xiaomi.git -b lineage-21 vendor/xiaomi
+git clone --depth=1 https://github.com/Amrito-Projects/vendor_xiaomi.git -b lineage-21 vendor/xiaomi
 
-git clone --depth=1 https://github.com/Stone-Trees/kernel_xiaomi_sm6375.git -b lineage-20 kernel/xiaomi/sm6375
+git clone --depth=1 https://github.com/Stone-Trees/kernel_xiaomi_sm6375.git -b 14 kernel/xiaomi/sm6375
 
-git clone --depth=1 https://github.com/Stone-Trees/xiaomi_stone-kernel.git device/xiaomi/stone-kernel
+git clone --depth=1 https://github.com/Stone-Trees/xiaomi_stone-kernel.git -b 14-old  device/xiaomi/stone-kernel
 
 git clone --depth=1 https://github.com/stone-devs/hardware_xiaomi hardware/xiaomi
-
-git clone --depth=1 https://github.com/Drpto/packages_apps_prebuilts.git packages/apps/prebuilts
